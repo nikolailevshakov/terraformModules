d4f0f809774dabac42def03090b04105f5b4bdee
Levshakov Nikolai <levshakov_nikk@bk.ru>
2023-06-06 12:29:47 +0200

    sitespeed-io: modified startup script, run influx and grafana in docker. Need to provision datasource and dashboards in grafana

--- modules/sitespeed-io/userdata-monitoring.sh
+++ modules/sitespeed-io/userdata-monitoring.sh
@@ -2,45 +2,56 @@
 
 
 apt update -y
-apt install jq -y
-wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.7.0-amd64.deb
-dpkg -i influxdb2-2.7.0-amd64.deb
-service influxdb start
 
-wget https://dl.influxdata.com/influxdb/releases/influxdb2-client-2.7.3-linux-amd64.tar.gz
-tar xvzf influxdb2-client-2.7.3-linux-amd64.tar.gz
-cp influx /usr/local/bin/
+# INSTALL DOCKER
 
-influx setup --username admin --password adminadmin --org local -bucket default -f
-influx auth list -u admin --hide-headers --json | jq '.[0].token' > influx_token.txt
-export INFLUX_TOKEN=$${echo influx_token.txt}
+apt-get update -y
+apt-get install ca-certificates curl gnupg
 
-apt-get install -y apt-transport-https
-apt-get install -y software-properties-common
-wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
-echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
-apt-get update
-apt-get install grafana
-systemctl start grafana-server
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# RUN INFLUXB
+
+docker run -d --name influxdb -p 8086:8086 --volume $PWD/influxdb:/var/lib/influxdb2 influxdb:2.7.0
+sleep 5
+docker exec influxdb bash -c "influx setup --username admin --password adminadmin --org local -bucket default -f"
+docker exec influxdb bash -c "influx auth list -u admin --hide-headers --json | grep token | cut -c 13-100 > influx_token.txt"
+
+docker cp influxdb:/influx_token.txt /home/ubuntu/influxdb_token.txt
+export INFLUXDB_TOKEN=$$(cat influxdb)
+
+# RUN GRAFANA
+
+docker run -d --name grafana -p 3000:3000 grafana/grafana-oss
 
 
-cat << EOF > /etc/grafana/grafana.ini
-apiVersion: 1
-
-datasources:
-  - name: InfluxDB_v2_Flux
-    type: influxdb
-    access: proxy
-    url: http://localhost:8086
-    jsonData:
-      version: Flux
-      organization: local
-      defaultBucket: default
-      tlsSkipVerify: true
-    secureJsonData:
-      token: $INFLUX_TOKEN
-EOF
-
-chmod 600 ~/.ssh/authorized_keys
-chmod 644 ~/.ssh/id_rsa.pub
+#systemctl start grafana-server
+#
+#
+#cat << EOF > /etc/grafana/grafana.ini
+#apiVersion: 1
+#
+#datasources:
+#  - name: InfluxDB_v2_Flux
+#    type: influxdb
+#    access: proxy
+#    url: http://localhost:8086
+#    jsonData:
+#      version: Flux
+#      organization: local
+#      defaultBucket: default
+#      tlsSkipVerify: true
+#    secureJsonData:
+#      token: $INFLUX_TOKEN
+#EOF
